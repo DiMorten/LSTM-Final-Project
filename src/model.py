3b1b5771e95d6044d95f9293a9a1cdeaa7862ac1
Jorge Andres Chamorro Martinez <cmjorgeandres@gmail.com>
2018-06-03 20:56:38 -0300

    Added super classes

--- src/model.py
+++ src/model.py
@@ -26,34 +26,11 @@ import pickle
 import utils
 import deb
 np.set_printoptions(suppress=True)
-class neural_network(object):
-	def average_accuracy_get(self,target,prediction):
-		correct_per_class=np.zeros(self.n_classes).astype(np.float32)
-		for clss in range(0,self.n_classes):
-			correct_per_class[clss]=float(np.count_nonzero(np.equal(target[:,clss],prediction[:,clss])))/float(target.shape[0])
-		correct_per_class_average = np.average(correct_per_class)
 
-		return correct_per_class_average
-	def layer_lstm_get(self,data,filters,kernel):
-		#filters=64
-		cell = tf.contrib.rnn.ConvLSTMCell(2,self.shape + [self.channels], filters, kernel)
-		val, state = tf.nn.dynamic_rnn(cell, data, dtype=tf.float32)
-		if self.debug: deb.prints(val.get_shape)
-		last = tf.gather(val, int(val.get_shape()[1]) - 1,axis=1)
-		if self.debug: deb.prints(last.get_shape())
-		return last
-	def loss_optimizer_set(self,target,prediction):
-		# Estimate loss from prediction and target
-		cross_entropy = -tf.reduce_sum(target * tf.log(tf.clip_by_value(prediction,1e-10,1.0)))
+# ===================================NeuralNet generic class ======================================================= #
+# =================================== Might take onehot or image output ============================================= #
+class NeuralNet(object):
 
-		# Prepare the optimization function
-		optimizer = tf.train.AdamOptimizer()
-		minimize = optimizer.minimize(cross_entropy)
-		return minimize
-
-
-
-class conv_lstm(neural_network):
 	def __init__(self, sess, batch_size=50, epoch=200, train_size=1e8,
                         timesteps=utils.conf["t_len"], shape=[32,32],
                         kernel=[3,3], channels=6, filters=32, n_classes=9,
@@ -70,30 +47,49 @@ class conv_lstm(neural_network):
 		self.filters = filters
 		self.n_classes = n_classes
 		self.checkpoint_dir = checkpoint_dir
-		self.conf={'mode':1}
+		self.conf=utils.conf
 		self.debug=1
-		self.model_build()
-	def model_build(self):
-		self.data = tf.placeholder(tf.float32, [None] +[self.timesteps] + self.shape + [self.channels])
-		self.target = tf.placeholder(tf.float32, [None, self.n_classes])
-		if self.debug: deb.prints(self.target.get_shape)
 
-		self.prediction = self.model_graph_get(self.data)
+	def layer_lstm_get(self,data,filters,kernel):
+		#filters=64
+		cell = tf.contrib.rnn.ConvLSTMCell(2,self.shape + [self.channels], filters, kernel)
+		val, state = tf.nn.dynamic_rnn(cell, data, dtype=tf.float32)
+		if self.debug: deb.prints(val.get_shape)
+		last = tf.gather(val, int(val.get_shape()[1]) - 1,axis=1)
+		if self.debug: deb.prints(last.get_shape())
+		return last
 
-		# Set optimizer
-		self.minimize=self.loss_optimizer_set(self.target,self.prediction)
-		self.mistakes = tf.not_equal(tf.argmax(self.target, 1), tf.argmax(self.prediction, 1))
-		self.error = tf.reduce_mean(tf.cast(self.mistakes, tf.float32))
-		
-		self.error_sum = tf.summary.scalar("error", self.error)
-
+	def tensorboard_saver_init(self, error):
+		error_sum = tf.summary.scalar("error", error)		
+		saver = tf.train.Saver(max_to_keep=4, keep_checkpoint_every_n_hours=2)
+		merged = tf.summary.merge_all()
+		return error_sum, saver, merged
+	def trainable_vars_print(self):
 		t_vars = tf.trainable_variables()
-
-		self.saver = tf.train.Saver(max_to_keep=4, keep_checkpoint_every_n_hours=2)
-		self.merged = tf.summary.merge_all()
-
 		if self.debug: print("trainable parameters",np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()]))
+# ============================ NeuralNet takes onehot image output ============================================= #
+class NeuralNetOneHot(NeuralNet):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
 
+	def average_accuracy_get(self,target,prediction):
+		correct_per_class=np.zeros(self.n_classes).astype(np.float32)
+		for clss in range(0,self.n_classes):
+			correct_per_class[clss]=float(np.count_nonzero(np.equal(target[:,clss],prediction[:,clss])))/float(target.shape[0])
+		correct_per_class_average = np.average(correct_per_class)
+
+		return correct_per_class_average
+	def loss_optimizer_set(self,target,prediction):
+		# Estimate loss from prediction and target
+		cross_entropy = -tf.reduce_sum(target * tf.log(tf.clip_by_value(prediction,1e-10,1.0)))
+
+		# Prepare the optimization function
+		optimizer = tf.train.AdamOptimizer()
+		minimize = optimizer.minimize(cross_entropy)
+
+		mistakes = tf.not_equal(tf.argmax(target, 1), tf.argmax(prediction, 1))
+		error = tf.reduce_mean(tf.cast(mistakes, tf.float32))
+		return minimize, mistakes, error
 	def train(self, args):
 		#init_op = tf.initialize_all_variables()
 		init_op = tf.global_variables_initializer()
@@ -102,7 +98,7 @@ class conv_lstm(neural_network):
 #		self.writer = tf.summary.FileWriter(utils.conf["summaries_path"], graph=tf.get_default_graph())
 		self.writer = tf.summary.FileWriter(utils.conf["summaries_path"], self.sess.graph)
 
-		data = self.data_load(utils.conf)
+		data = self.data_load(self.conf)
 		batch_idxs = min(len(data["train"]["im_paths"]), args.train_size) // self.batch_size
 		deb.prints(data["train"]["labels"].shape)
 		deb.prints(data["test"]["labels"].shape)
@@ -141,7 +137,7 @@ class conv_lstm(neural_network):
 
 
 		print("Model restored.")
-		data = self.data_load(utils.conf)
+		data = self.data_load(self.conf)
 		batch_idxs = min(len(data["train"]["im_paths"]), args.train_size) // self.batch_size
 		idx=1
 		batch_file_paths = data["train"]["im_paths"][idx*self.batch_size:(idx+1)*self.batch_size]
@@ -156,11 +152,10 @@ class conv_lstm(neural_network):
 		deb.prints(average_accuracy)
 		self.model_test_on_samples(data)
 
-	def model_test_on_samples(self,dataset):
+	def model_test_on_samples(self,dataset,sample_range=range(15,20)):
 
 		print("train results")
 		
-		sample_range=range(15,20)
 		print(np.around(self.sess.run(self.prediction,{self.data: dataset["train"]["ims"][sample_range]}),decimals=4))
 		deb.prints(dataset["train"]["labels"][sample_range])
 		
@@ -188,6 +183,27 @@ class conv_lstm(neural_network):
 		data["test"]["labels"] = np.load(conf["test"]["balanced_path_label"]+"labels.npy")
 		data["test"]["ims"]=[np.load(im_path) for im_path in data["test"]["im_paths"]]
 		return data
+
+# ================================= Implements ConvLSTM ============================================== #
+class conv_lstm(NeuralNetOneHot):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.model_build()
+
+	def model_build(self):
+		self.data,self.target=self.placeholder_init(self.timesteps,self.shape,self.channels,self.n_classes)
+		self.prediction = self.model_graph_get(self.data)
+
+		# Set optimizer
+		self.minimize,self.mistakes,self.error=self.loss_optimizer_set(self.target,self.prediction)
+		self.error_sum, self.saver, self.merged = self.tensorboard_saver_init(self.error)
+		self.trainable_vars_print()
+		
+	def placeholder_init(self,timesteps,shape,channels,n_classes):
+		data = tf.placeholder(tf.float32, [None] +[timesteps] + shape + [channels])
+		target = tf.placeholder(tf.float32, [None, n_classes])
+		if self.debug: deb.prints(target.get_shape())
+		return data,target
 	def model_graph_get(self,data):
 		graph_pipeline=self.layer_lstm_get(data,filters=self.filters,kernel=self.kernel)
 		
@@ -201,8 +217,5 @@ class conv_lstm(neural_network):
 		graph_pipeline = tf.layers.dense(graph_pipeline, self.n_classes,activation=tf.nn.softmax)
 		if self.debug: deb.prints(graph_pipeline.get_shape())
 		return graph_pipeline
-
-
-
 
 
