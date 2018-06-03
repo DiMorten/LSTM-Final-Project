00c72f076e150954695babc69be1f44867583964
Jorge Andres Chamorro Martinez <cmjorgeandres@gmail.com>
2018-06-03 01:05:48 -0300

    Samples per class conf

--- src/utils.py
+++ src/utils.py
@@ -611,7 +611,9 @@ if __name__ == "__main__":
 			samples_per_class=500
 		else:
 			samples_per_class=1500
-		data["train"]["ims"],data["train"]["labels"],data["train"]["labels_onehot"]=data_balance(conf,data,samples_per_class)
+
+		samples_per_class=1500
+		data["train"]["ims"],data["train"]["labels"],data["train"]["labels_onehot"]=data_balance(conf,data,conf["balanced"]["samples_per_class"])
 		data["train"]["n"]=data["train"]["ims"].shape[0]
 
 		deb.prints(data["train"]["ims"].shape)
